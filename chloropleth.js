c7e3bfdde59d0a057899800637e6c251d6d43c4f
Alekya Popuri <alekyapopuri96@gmail.com>
2020-12-11 15:13:45 -0500

    Delete chloropleth.js

--- chloropleth.js
+++
@@ -1,7 +0,0 @@
-import {legend} from "@d3/color-legend"
-import {legend} from @d3/color-legend
-d3 = require("d3@6")
-d3 =
-
-    Object {format: ƒ(t), formatPrefix: ƒ(t, n), timeFormat: ƒ(t), timeParse: ƒ(t), utcFormat: ƒ(t), utcParse:
-
